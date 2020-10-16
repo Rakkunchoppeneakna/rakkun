3af2da04163ceb5612c513f1c5fdb26c93724cea
Rakkunchoppeneakna <72967225+Rakkunchoppeneakna@users.noreply.github.com>
2020-10-16 15:13:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 
 $API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = 'ouhqskdRP/sUP8uwpjAadPDJz6rj1Y3IR0/ZznmHBgsPmYq6Q+hzdEJ4OXgyw/8NaLy6GLAZYYbLhF/7S6i8K07k3yxT0sWcMEa6ixgJ2c0XIOEKRfUEQAsHVi4PbQU4HEk9GOq/cmdR3iRkQE9e5gdB04t89/1O/w1cDnyilFU='; 
-$channelSecret = 'ba6e01c3eb0671a32e7d9fb3dbabd67d';
+$ACCESS_TOKEN = 'D5aFH7G28Jc4CEXJoVcM5RF/YxCmxPs3LermBmLtvaIWeAdp4tErcwAYtRp1FulJ8RaNVEfChDQtpUbwgnnjXShit02Tbi6eQg/7zq/ReuKhbZz9MS9mf7MTZTEO9VtjjF6c4OCeQzKBC7D+gb8DIwdB04t89/1O/w1cDnyilFU='; 
+$channelSecret = '9aae7ef4e3aee9369ce5989dfff4f707';
 
 
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
